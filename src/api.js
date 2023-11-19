e7b21b2f6bce9ebc54e5f02e1395b98fba9932e6
corto0406 <132603219+corto0406@users.noreply.github.com>
2023-11-19 11:05:49 +0100

    immersion 4.9

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 import mockData from './mock-data';
+import NProgress from 'nprogress';
 
 /**
  *
@@ -61,6 +62,11 @@ export const getEvents = async () => {
     return mockData.items;
   }
 
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return events ? JSON.parse(events) : [];
+  }
   const token = await getAccessToken();
   if (token) {
     removeQuery();
@@ -69,8 +75,11 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      NProgress.done();
+      localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null;
+
   }
 };
 
